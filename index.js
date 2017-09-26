cef2769b7aaf05da32c4c26b2ffb6d5bbbc9382b
big-armand <a.falabregues@gmail.com>
2017-09-26 12:12:57 +0200

    todoList works live

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ var express = require('express'),
 	ent = require('ent'),
 	fs = require('fs');
 
-var json = {
-	marque: 'Lamborgini',
-	modele: 'Gallardo'
-};
+app.set('socketio', io);
+var jsonTodo = null;
+
+app.ioTodo = io.of('/todo');
 
 io.of('/chat').on("connection", function (socket) {
 	socket.on('pseudo', function (clientName) {
@@ -29,11 +29,14 @@ io.of('/chat').on("connection", function (socket) {
 });
 
 io.of('/todo').on("connection", function (socket) {
-	var todoList = fs.readFileSync('todo.json', 'utf-8'),
+	var todoList = fs.readFileSync('todo.json', 'utf-8');
+	if (todoList != null) {
 		jsonTodo = JSON.parse(todoList);
-	jsonTodo.forEach(function (item) {
-		io.of('/todo').to(socket.id).emit('addOneTodo', item);
-	});
+		jsonTodo.forEach(function (item) {
+			io.of('/todo').to(socket.id).emit('addOneTodo', item);
+		});
+	}
+
 
 	var tobuyList = fs.readFileSync('tobuy.json', 'utf-8'),
 		jsonTobuy = JSON.parse(tobuyList);
@@ -42,12 +45,20 @@ io.of('/todo').on("connection", function (socket) {
 	});
 
 	socket.on('addTodo', function (item) {
-		jsonTodo.push(item);
-		todoList = JSON.stringify(jsonTodo);
-		fs.writeFile('todo.json', todoList, 'utf8', function (err) {
-			if (err) throw err;
-		});
-		io.of('/todo').emit('addOne', item);
+		var todoList = fs.readFileSync('todo.json', 'utf-8');
+		if (todoList != null) {
+			jsonTodo = JSON.parse(todoList);
+			jsonTodo.push(item);
+			todoList = JSON.stringify(jsonTodo);
+			fs.writeFile('todo.json', todoList, 'utf8', function (err) {
+				if (err) throw err;
+			});
+			io.of('/todo').emit('addOneTodo', item);
+		}
+	});
+
+	socket.on('changeTodo', function () {
+		io.of('/todo').emit('changeTodo');
 	});
 
 });
@@ -71,6 +82,25 @@ app.use(express.static(__dirname + '/chat'))
 	/* affiche la todolist */
 	.get('/todo', function (req, res) {
 		res.sendFile(__dirname + '/todoList/todo.html');
+	})
+
+	.get('/todo/suppr/:id', function (req, res) {
+		if (req.params.id != "") {
+			jsonTodo = fs.readFileSync('todo.json', 'utf-8');
+			if (jsonTodo != null) {
+				console.log(req.params.id);
+				var arrayTodo = JSON.parse(jsonTodo);
+				arrayTodo.splice(req.params.id, 1);
+				jsonTodo = JSON.stringify(arrayTodo);
+				fs.writeFile('todo.json', jsonTodo, 'utf8', function (err) {
+					if (err) throw err;
+				});
+			}
+		}
+		req.app.ioTodo.emit('changeTodo', req.params.id);
+		/*var insideIo = req.app.get('socketio');
+		insideIo.broadcast.emit('changeTodo');*/
+		res.redirect("/todo");
 	})
 
 	/* redirige vers l'accueil si la page demandée n'est pas trouvée */
